a93380b1e15ca2b9604675ccd6d5566910f7d94d
Allan mang'eni <allanwanyonyi@protonmail.com>
2023-03-23 11:50:53 +0000

    balance checks

--- test/IERC20.ts
+++ test/IERC20.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { WRDAO } from '../typechain-types';
+import { WRDAO, WRDAO__factory } from '../typechain-types';
 import { Contract, Signer } from 'ethers';
 
 describe ("WRDAO Token Contract", () => {
@@ -21,5 +21,50 @@ describe ("WRDAO Token Contract", () => {
     expect(await wrdao.symbol()).to.equal("WRDAO");
     expect(await wrdao.decimals()).to.equal(18);
   });
-})
+
+  it("return correct contract supply", async () => {
+    expect(await wrdao.totalSupply()).to.equal(1000000000 * 10 ** 18);
+  });
+
+  it("should initiate transfer tokens", async () => {
+    const initialBalance = await wrdao.balanceOf(await bob.getAddress());
+    await wrdao.connect(owner).transfer(await alice.getAddress(), 100);
+    const finalBalance = await wrdao.balanceOf(await bob.getAddress());
+    expect(finalBalance).to.equal(initialBalance.add(100));
+  });
+
+  it("approve and transferFrom tokens", async () => {
+    await wrdao.connect(owner).approve(await bob.getAddress(), 100);
+    await wrdao.connect(alice).transferFrom(
+      await owner.getAddress(),
+      await bob.getAddress(),
+      100
+    );
+    const bobBalance = await wrdao.balanceOf(await bob.getAddress());
+    expect(bobBalance).to.equal(100);
+  });
+
+  it("!transfer tokens with insufficient balance", async () => {
+    const initialBalance = await wrdao.balanceOf(await owner.getAddress());
+    await expect(
+      wrdao.connect(owner).transfer(await bob.getAddress(), initialBalance.add(1))
+    ).to.be.revertedWith("Transfer balance");
+    const finalBalance = await wrdao.balanceOf(await owner.getAddress());
+    expect(finalBalance).to.equal(initialBalance);
+  });
+
+  it("!transfer tokens with insufficient allowance", async () => {
+    const initialBalance = await wrdao.balanceOf(await owner.getAddress());
+    await wrdao.connect(owner).approve(await bob.getAddress(), 100);
+    await expect(
+      wrdao.connect(bob).transferFrom(
+        await owner.getAddress(),
+        await alice.getAddress(),
+        initialBalance.add(1)
+      )
+    ).to.be.revertedWith("no allowance");
+    const finalBalance = await wrdao.balanceOf(await owner.getAddress());
+    expect(finalBalance).to.equal(initialBalance);
+  });
+});
 
