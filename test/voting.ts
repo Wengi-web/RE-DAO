f8f2f63228efc16efb7ecf5bc8cefdf1e8072d82
Allan mang'eni <allanwanyonyi@protonmail.com>
2023-03-26 20:16:28 +0000

    add test allow function

--- test/voting.ts
+++ test/voting.ts
@@ -1,5 +1,8 @@
 import { ethers } from "hardhat";
 import { Contract, Signer } from "ethers";
+import { Voting, Voting__factory } from '../typechain-types';
+import { Proposal, Proposal__factory} from '../typechain-types';
+
 import { expect } from "chai";
 
 describe("Voting Contract", () => {
@@ -14,7 +17,7 @@ describe("Voting Contract", () => {
     //new proposal
     beforeEach(async () => {
         [owner, addr1, addr2] = await ethers.getSigners();
-        const proposal = await ethers.getContractFactory("Proposal");
+        let proposal = await ethers.getContractFactory("Proposal");
         proposal = await proposal.connect(owner).deploy(
             1,
             await owner.getAddress(),
@@ -22,5 +25,21 @@ describe("Voting Contract", () => {
             "This is a test proposal",
             ethers.utils.parseEther("1")
         );
+        await proposal.deployed();
+        const Voting = await ethers.getContractFactory("Voting");
+        voting = await voting.connect(owner).deploy(
+            proposal.address,
+            await owner.getAddress()
+        );
+        await voting.deployed();
+        daotoken = await ethers.getContractAt("WRDAO", await owner.getAddress());
+        await daoToken.connect(owner).approve(voting.address, ethers.utils.parseEther("1"));
+    });
+
+    it("should !allow users to vote more than once", async () => {
+        await voting.connect(addr1).vote(true, ethers.utils.parseEther("0.5"));
+        await expect(
+            voting.connect(addr1).vote(false, ethers.utils.parseEther("0.5"))
+        ).to.be.revertedWith("")
     })
 })
